2c55139793e8e315bee25b82a5ae05e4c894e6e7
Jacob Prud'homme <jacob.pkmn@gmail.com>
2015-10-16 13:11:54 -0400

    Lil fixes
    Sry I couldn't wait, merge em

--- trafficLights/trafficLights.ino
+++ trafficLights/trafficLights.ino
@@ -8,7 +8,7 @@ long interval = 300;	//blink interval for pedestrians
 int ledState = LOW;
 int state;
 int i = 0;
- 
+
 void setup() {
   for(int i = 0; i < 5; i++) {
 	  pinMode(trafficLights1[i], OUTPUT);
@@ -16,26 +16,26 @@ void setup() {
   }
 	Serial.begin(9600);
 }
- 
-void loop() {	
-	unsigned long currentMillis = millis();		
-	if(currentMillis - previousCars < duration[i]) {			
-		situation(i);		
-	} else { 
-		previousCars = currentMillis; 
+
+void loop() {
+	unsigned long currentMillis = millis();
+	if(currentMillis - previousCars < duration[i]) {
+		situation(i);
+	} else {
+		previousCars = currentMillis;
 		if(i >= situations) {
 			i = 0;
 		} else {
 			i++;
-		}		
+		}
 	}
 }
- 
-void activateTrafficLight1(String lights, int pedestrians) {	
+
+void activateTrafficLight1(String lights, int pedestrians) {
 	for(int x = 0; x < 3; x++) {
 		if(lights[x] == '0') state = LOW;
 		if(lights[x] == '1') state = HIGH;
-		digitalWrite(trafficLights1[x], state);	
+		digitalWrite(trafficLights1[x], state);
 	}
 	if(pedestrians == 1) {
     digitalWrite(trafficLights1[4], LOW);
@@ -45,8 +45,8 @@ void activateTrafficLight1(String lights, int pedestrians) {
     digitalWrite(trafficLights1[4], HIGH);
 	}
 }
- 
-void activateTrafficLight2(String lights, int pedestrians) {	
+
+void activateTrafficLight2(String lights, int pedestrians) {
 	for(int x = 0; x < 3; x++) {
 		if(lights[x] == '0') state = LOW;
 		if(lights[x] == '1') state = HIGH;
@@ -60,36 +60,37 @@ void activateTrafficLight2(String lights, int pedestrians) {
 		digitalWrite(trafficLights2[3], LOW);
 	}
 }
- 
+
 void situation(int i) {
 	switch(i) {
-		case 0: 
+		case 0:
 			activateTrafficLight1("100",1); // 100 means red ON, yellow OFF, green OFF
 			activateTrafficLight2("001",0); // the second parameter is for pedestrians
 			break;							// 1 is ON and 0 is OFF
-		case 1: 
+		case 1:
 			activateTrafficLight1("110",0); // 110: red ON, yellow ON, green OFF
 			activateTrafficLight2("010",0);
-			break;	
-		case 2: 
+			break;
+		case 2:
 			activateTrafficLight1("001",0);
 			activateTrafficLight2("100",1);
 			break;
-		case 3:	
+		case 3:
 			activateTrafficLight1("010",0);
 			activateTrafficLight2("110",0);
-			break;	
+			break;
 	}
 }
- 
+
 void blinkPed(int ped) {
 	unsigned long currentMillis = millis();
-	if(currentMillis - previousPeds > interval) {		
-		previousPeds = currentMillis;		
-		if (ledState == LOW)
-		ledState = HIGH;
-		else
-		ledState = LOW;
-		digitalWrite(ped, ledState);
-	}	
+	if(currentMillis - previousPeds > interval) {
+		previousPeds = currentMillis;
+		if (ledState == LOW) {
+		  ledState = HIGH;
+		} else {
+		  ledState = LOW;
+		  digitalWrite(ped, ledState);
+	  }
+	}
 }
