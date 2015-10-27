7882f8e12a51d2ac12cfc8bf57f8cf33c6eea0d5
Jacob Prud'homme <jacob.pkmn@gmail.com>
2015-10-27 11:02:38 -0400

    Commented IT ALL!!!
    New + improved

--- trafficLights/trafficLights.ino
+++ trafficLights/trafficLights.ino
@@ -1,3 +1,4 @@
+/*
 int trafficLights1[] = {4, 5, 6, 7, 8};	// red, yel0, green, pedestrians led pins
 int trafficLights2[] = {9, 10, 11, 12, 13};	// red, yel0, green, pedestrians led pins
 int sensors1 = 2; // pin for the main way sensors
@@ -61,18 +62,16 @@ void loop() {
       Serial.print("Rev: ");
       Serial.print(test);
       test.toCharArray(lights, 9);
-      /*
-      for(int i = 0; i < 5; i++) {
-        digitalWrite(trafficLights1[i], HIGH);
-      }
-      */
+      //for(int i = 0; i < 5; i++) {
+      //  digitalWrite(trafficLights1[i], HIGH);
+      //}
       //REPLACE THIS
     } else {
       for(int i = 0; i < 5; i++) {
         digitalWrite(trafficLights1[i], LOW);
       }
     }
-     
+
     // confirm values received in serial monitor window
     Serial.print("--Arduino received: ");
     Serial.println(recv);
@@ -234,3 +233,71 @@ void blinkPed2(int ped) {
 
 //TURN ON/OFF LIGHTS LOCALLY AND SEND DATA
 //ADD SEND OF SENSOR DATA
+*/
+
+void setup() {
+  for(int i = 0; i < 5; i++) {
+    pinMode(trafficLights1[i], OUTPUT);
+    pinMode(trafficLights2[i], OUTPUT);
+  }
+
+  pinMode(sensors1, INPUT);
+  pinMode(sensors2, INPUT);
+
+  attachInterrupt(digitalPinToInterrupt(2), addCars1, RISING); // activate an external interrupt on the rising edge of pin 2 MIGHT HAVE TO CHANGE TO FALLING
+  attachInterrupt(digitalPinToInterrupt(3), addCars2, RISING); // activate an external interrupt on the rising edge of pin 3 MIGHT HAVE TO CHANGE TO FALLING
+  
+  Serial.begin(9600);
+}
+
+// to be activated asynchronously if pin 2 passes from 0 to 1
+void addCars1() {
+  Serial.write(2); // send signal to server; a car passed on mainway
+}
+
+// to be activated asynchronously if pin 3 passes from 0 to 1
+void addCars2() {
+  Serial.write(3); // send signal to server; a car passed on throughway
+}
+
+void loop() {
+  // while there is no serial connection, flash red lights to treat as 4-way stop
+  while (!Serial) {
+    for(int i = 0; i < 5; i++) {
+      digitalWrite(trafficLights1[i], LOW);
+      digitalWrite(trafficLights2[i], LOW);
+    }
+    delay(2500);
+    digitalWrite(trafficLights1[0], HIGH);
+    digitalWrite(trafficLights2[0], HIGH);
+    delay(2500);
+  }
+  
+  //// FIX THIS
+  // if serial port is available, read incoming bytes
+  if (Serial.available() > 0) {
+    recv = Serial.read();
+ 
+    // if 'y' (decimal 121) is received, turn LED/Powertail on
+    // anything other than 121 is received, turn LED/Powertail off
+    if (recv == 1){
+      String test = "";
+      test = Serial.readStringUntil('\n');
+      Serial.print("Rev: ");
+      Serial.print(test);
+      test.toCharArray(lights, 9);
+      //for(int i = 0; i < 5; i++) {
+      //  digitalWrite(trafficLights1[i], HIGH);
+      //}
+      //REPLACE THIS
+    } else {
+      for(int i = 0; i < 5; i++) {
+        digitalWrite(trafficLights1[i], LOW);
+      }
+    }
+
+    // confirm values received in serial monitor window
+    Serial.print("--Arduino received: ");
+    Serial.println(recv);
+  }
+}
