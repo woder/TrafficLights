f6da1dab6a105d66b493f9412e5bc173857675f8
Jacob Prud'homme <jacob.pkmn@gmail.com>
2015-11-17 09:50:50 -0500

    ayy lmao

--- trafficLights/trafficLights.ino
+++ trafficLights/trafficLights.ino
@@ -1,46 +1,31 @@
-int trafficLightPins[] = {4, 5, 6, 9, 10, 11, 7, 8, 12, 13}; //green, yellow, red; green yellow red; ped blue ped orange; ped blue ped orange
-int sensor1 = 2; // pin for the main way sensors
-int sensor2 = 3; // pin for the cross way sensors
+int trafficLightPins[] = {4, 5, 6, 9, 10, 11, 7, 8, 12, 13}; // Vert, jaune, rouge; vert, jaune, rouge; bleu, orange; bleu, orange
+int sensor1 = 2;                                             // Pin pour les senseurs sur la voie principale
+int sensor2 = 3;                                             // Pin pour les senseurs sur la voie transversale
 long previousPeds = 0;
-long interval = 300;	//blink interval for pedestrians
-int ledState = 0; // temp var for switching light state
+long interval = 300;	                                       // Durée de l'intervalle de clignotement de la lumière pour piétons
+int ledState = 0;                                            // Variable temporaire pour changer l'état des DELs
 int i = 0;
-int recv = 0; // byte received on the serial port
-boolean pedtoggle = false; //are we toggling the ped1
-boolean ped2toggle = false; //are we toggling the ped2
-int pedstate = 0;
-int ped2state = 0;
-long sensorData1 = 0;
-long sensorData2 = 0;
+int recv = 0;                                                // Octet reçu sur la connexion en série
+boolean pedtoggle = false;                                   // Faire clignoter ou non la lumière pour piétons sur la voie principale
+boolean ped2toggle = false;                                  // Faire clignoter ou non la lumière pour piétons sur la voie transversale
+int pedstate = 0;                                            // État de la lumière pour piétons sur la voie principale
+int ped2state = 0;                                           // État de la lumière pour piétons sur la voie transversale
+long sensorData1 = 0;                                        // État analogue du senseur piézo sur la voie principale
+long sensorData2 = 0;                                        // État analogue du senseur piézo sur la voie transversale
 
 void setup() {
   for(int i = 0; i < 10; i++) {
-    pinMode(trafficLightPins[i], OUTPUT);
+    pinMode(trafficLightPins[i], OUTPUT); // Initialiser tous les pins des DELs en tant que sortie
   }
 
-  pinMode(sensor1, INPUT);
-  pinMode(sensor2, INPUT);
+  pinMode(sensor1, INPUT);                // Initialiser le pin du senseur sur la voie principale en tant qu'entrée
+  pinMode(sensor2, INPUT);                // Initialiser le pin du senseur sur la voie transversale en tant qu'entrée
   
-  Serial.begin(9600);
+  Serial.begin(9600);                     // Commencer la connexion en série à 9600Bd
 }
 
 void loop() {
-  /*
-  // while there is no serial connection, flash red lights to treat as 4-way stop
-  while (!Serial) {
-    unsigned long timeoutTimer = millis();
-
-    if(timeoutTimer >= 5000) {
-      for(int i = 0; i < 10; i++) {
-        digitalWrite(trafficLightPins[i], LOW);
-      }
-      delay(2500);
-      digitalWrite(trafficLightPins[2], HIGH);
-      digitalWrite(trafficLightPins[5], HIGH);
-      delay(2500);
-    }
-  }
-  */
+  unsigned long sensorCheck = millis();
   
   blinkPed1(trafficLightPins[7]);
   blinkPed2(trafficLightPins[9]);
@@ -110,20 +95,22 @@ void loop() {
     }
   }
 
-  sensorData1 = analogRead(sensor1);
-  sensorData1 = map(sensorData1, 0, 1023, 0, 255);
-  sensorData1 = constrain(sensorData1, 0, 255);
-  sensorData2 = analogRead(sensor2);
-  sensorData2 = map(sensorData2, 0, 1023, 0, 255);
-  sensorData2 = constrain(sensorData2, 0, 255);
-  //Serial.println(sensorData1);
-  if(sensorData1 > 127) {
-    Serial.write(2);
-    Serial.write(sensorData1);
-  }
-  if(sensorData2 > 127) {
-    Serial.write(3);
-    Serial.write(sensorData2);
+  if(sensorCheck%1000 == 0) {
+    sensorData1 = analogRead(sensor1);
+    sensorData1 = map(sensorData1, 0, 1023, 0, 255);
+    sensorData1 = constrain(sensorData1, 0, 255);
+    sensorData2 = analogRead(sensor2);
+    sensorData2 = map(sensorData2, 0, 1023, 0, 255);
+    sensorData2 = constrain(sensorData2, 0, 255);
+    //Serial.println(sensorData1);
+    if(sensorData1 > 127) {
+      Serial.write(2);
+      Serial.write(sensorData1);
+    }
+    if(sensorData2 > 127) {
+      Serial.write(3);
+      Serial.write(sensorData2);
+    }
   }
 }
 
